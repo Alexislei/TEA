659f1a8d26bce1872f72b0eb860ae6bb4de977db
Alexislei <54882617+Alexislei@users.noreply.github.com>
2023-10-27 14:37:51 +0800

    Update runAlgorithm.m

--- code/runAlgorithm.m
+++ code/runAlgorithm.m
@@ -1,13 +1,19 @@
 clear all
 clc
 
-runtimes=30;
-cost_be=zeros(runtimes,1);
+% Configure any of these parameters to match needs of given problems
 
-for a=1:runtimes
-    load data_kazeroon.mat
-    Ge=2000;
-    params=[200 Ge 0.04 0.6 0.08 0.01];
-    [cost_best,pop_per_gen]=main_func_n1_1(params,F,C);
-    save(['n1_1_',num2str(double(a)),'.mat'],'cost_best','pop_per_gen');
-end
+popsize=param(:,1); % population size P
+Gen=param(:,2); % generation size
+R=param(:,3); % the percentage of replication of well-performed chromosomes  5%
+pc=param(:,4); % crossover rate
+pml=param(:,5); % mutation rate local
+pmg=param(:,6); % mutation rate global
+
+
+load data_kazeroon.mat
+Ge=2000;
+params=[200 Ge 0.04 0.6 0.08 0.01];
+[cost_best,pop_per_gen]=main_func_n1_1(params,F,C);
+save(['n1_1_',num2str(double(a)),'.mat'],'cost_best','pop_per_gen');
+
