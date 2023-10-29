37b6dbe3266294f6f167897d7dfe9915cd19247b
Alexislei <54882617+Alexislei@users.noreply.github.com>
2023-10-29 23:02:27 +0800

    Update calc_cost.m

--- code/calc_cost.m
+++ code/calc_cost.m
@@ -1,11 +1,11 @@
 
-function [ fit ] = evaluate(pop,F,C,M)
+function [ fit ] = evaluate(pop, data)
 % Evaluation function: evaluate the fitness of each individual
-    [~,~,popsize]=size(pop);
-    fit=zeros(popsize,1); % fitness vector
-    for i=1:popsize 
-        cost=calc_fit(pop(:,:,i),M); % define the fitness function based on the target problem
-        fit(i,:)=cost;
+    popsize = size(pop, 3);
+    fit = zeros(popsize, 1); % fitness vector
+    for i = 1 : popsize 
+        cost = calc_fit(pop(:, :, I), M); % define the fitness function based on the target problem
+        fit(i,:) = cost;
     end
 end
 
