6493c8672c2af6746648af87f9e4fbf241c86bbf
Alexislei <54882617+Alexislei@users.noreply.github.com>
2023-10-29 23:00:59 +0800

    Update runAlgorithm.m

--- code/runAlgorithm.m
+++ code/runAlgorithm.m
@@ -2,12 +2,12 @@ clear all
 clc
 
 % Configure any of these parameters to match the needs of given problem
-params.gen=gen; % number of generations
-params.popsize=popsize; % population size
-params.m=m; params.n=n; % individual size
-params.pc=pc; % crossover rate
-params.pml=pml; % mutation rate local
-params.pmg=pmg; % mutation rate global
+params.gen = gen; % number of generations
+params.popsize = popsize; % population size
+params.m = m; params.n = n; % individual size
+params.pc = pc; % crossover rate
+params.pml = pml; % mutation rate local
+params.pmg = pmg; % mutation rate global
 
 data=load("data_kazeroon.mat");
 [fit_best,pop_best]=func_TEA(params,data); % require parameters and any problem-dependent data
